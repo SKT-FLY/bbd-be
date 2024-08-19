6bb0a7d950d06e43b495f6ceeb7f4c2183cdeede
seonguk.han <adulty22@gmail.com>
2024-08-19 18:26:54 +0900

    Feat: DB 연결 time out 설정

--- app/db/session.py
+++ app/db/session.py
@@ -9,7 +9,12 @@ load_dotenv()
 SQLALCHEMY_DATABASE_URL = os.environ.get("DATABASE_URL")
 
 # 비동기 SQLAlchemy 엔진을 생성
-engine = create_async_engine(SQLALCHEMY_DATABASE_URL, echo=True)
+engine = create_async_engine(
+    SQLALCHEMY_DATABASE_URL,
+    echo=True,
+    pool_recycle=28000,  # 28000초 후에 연결 재설정
+    connect_args={"connect_timeout": 20},  # 연결 시도 시 타임아웃 설정
+)
 
 # 비동기 세션을 설정
 SessionLocal = sessionmaker(
