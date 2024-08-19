53e02b149f08a706d91a23130c11096827517327
seonguk.han <adulty22@gmail.com>
2024-08-19 21:12:18 +0900

    Feat: Taxi-search response 변경

--- app/schemas/taxi.py
+++ app/schemas/taxi.py
@@ -1,5 +1,8 @@
 from pydantic import BaseModel
+from typing import Optional
+
 
 class TaxiSearchResponse(BaseModel):
-    taxi_phone: str
     fullAddress: str
+    type_1: Optional[str] = None
+    type_0: Optional[str] = None
