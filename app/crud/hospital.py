4f3369f50ea38057f9869c627fd3c722fc74dad2
seonguk.han <adulty22@gmail.com>
2024-08-23 11:20:42 +0900

    Feat: hospital query 수정

--- app/crud/hospital.py
+++ app/crud/hospital.py
@@ -1,11 +1,16 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
+from sqlalchemy import desc
 from app.models.hospital import Hospital
 from app.schemas.hospital import HospitalUpdate
 
 
 async def get_hospitals(user_id: int, db: AsyncSession):
-    result = await db.execute(select(Hospital).filter(Hospital.user_id == user_id))
+    result = await db.execute(
+        select(Hospital)
+        .filter(Hospital.user_id == user_id)
+        .order_by(desc(Hospital.visits_count))
+    )
     hospitals = result.scalars().all()
     return hospitals
 
@@ -26,6 +31,12 @@ async def update_or_create_hospital(
         hospital.visits_count = (
             hospital.visits_count + 1 if hospital.visits_count else 1
         )
+        hospital.hospital_name = hospital_data.hospital_name
+        hospital.hospital_phone = hospital_data.hospital_phone
+        hospital.hospital_type = hospital_data.hospital_type
+        hospital.hospital_address = hospital_data.hospital_address
+        hospital.hospital_centerLat = hospital_data.hospital_centerLat
+        hospital.hospital_centerLon = hospital_data.hospital_centerLon
     else:
         # 병원이 없는 경우 새로운 병원 추가
         hospital = Hospital(
@@ -33,7 +44,8 @@ async def update_or_create_hospital(
             hospital_phone=hospital_data.hospital_phone,
             hospital_type=hospital_data.hospital_type,
             hospital_address=hospital_data.hospital_address,
-            hospital_radius=hospital_data.hospital_radius,
+            hospital_centerLat=hospital_data.hospital_centerLat,
+            hospital_centerLon=hospital_data.hospital_centerLon,
             visits_count=1,
             user_id=user_id,  # user_id 설정
         )
